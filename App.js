41f7a549572ab3363d88b6baf2ffbf16d60679e9
Yauheni <yauheni.buturlia@yahoo.com>
2019-04-27 19:44:10 +0300

    New files like AppContainer and Reducer js

--- App.js
+++ App.js
@@ -1,49 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import * as React from 'react';
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from 'react-redux';
+//import axios from 'axios';
+//import axiosMiddleware from 'redux-axios-middleware';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import AppContainer from './AppContainer';
+import reducer from './Reducer';
+/*
+const client = axios.create({
+  baseURL: 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+  responseType: 'json'
 });
+*/
+const store = createStore(reducer);//, applyMiddleware(axiosMiddleware(client))
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
